945e302ab33510d6a17f2113ebf4ee068655c7dd
Shivakant6387 <shivakant5875@gmail.com>
2022-12-06 17:57:52 +0530

    Added file profile-specific and Hierarchical

--- src/main/java/com/example/RestAPIStartwithJAVA/Controller/RestEndPoint.java
+++ src/main/java/com/example/RestAPIStartwithJAVA/Controller/RestEndPoint.java
@@ -1,7 +1,10 @@
 package com.example.RestAPIStartwithJAVA.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 public class RestEndPoint {
@@ -10,11 +13,25 @@ public class RestEndPoint {
     private String cName;
     @Value("${default.course.chapterCount}")
     private int chaptersCount;
+    @Value("${course.chapterCount}")
+    private int getChaptersCount;
+    @Autowired
+    private CourseConfiguration courseConfiguration;
+
     @RequestMapping("/defaultCourse")
     public Course getDefaultCourse(@RequestParam (value = "name",defaultValue = " Spring Boot",required = false)String name,
                               @RequestParam (value = "chepterCount",defaultValue = " 2",required = false)int cheptersCount
     ){
         return new Course(cName,cheptersCount);
+    }
+    @RequestMapping("/gethierarchical")
+    public HashMap<String,Object> getConfigurationAnnotationProperties(){
+        HashMap<String,Object>resMap=new HashMap<String,Object>();
+        resMap.put("name",courseConfiguration.getName());
+        resMap.put("chaptersCount",courseConfiguration.getChapterCount());
+        resMap.put("rating",courseConfiguration.getRating());
+        resMap.put("author",courseConfiguration.getAuthor());
+        return resMap;
     }
     @RequestMapping("/course")
     public Course getEndPoint(@RequestParam (value = "name",defaultValue = " Spring Boot",required = false)String name,
